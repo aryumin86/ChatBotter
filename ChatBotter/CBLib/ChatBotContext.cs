0026616233e26c3a03c2dd458ee9616dded216f6
Alexey Ryumin <aryumin@aryumin.ru>
2018-02-02 22:47:57 +0300

    updates

--- ChatBotter/CBLib/ChatBotContext.cs
+++ ChatBotter/CBLib/ChatBotContext.cs
@@ -9,7 +9,7 @@ namespace CBLib
 {
     public class ChatBotContext : DbContext
     {
-        public ChatBotContext() : base("chatBotConn")
+        public ChatBotContext(DbContextOptions<ChatBotContext> options) : base(options)
         {
 
         }
@@ -22,24 +22,7 @@ namespace CBLib
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<TheProject>().ToTable("TheProjects");
-            modelBuilder.Entity<TheProject>().HasKey(t => t.Id);
-
-
-            modelBuilder.Entity<BotResponse>().ToTable("BotResponses");
-            modelBuilder.Entity<BotResponse>().HasKey(t => t.Id);
-
-
-            modelBuilder.Entity<Context>().ToTable("Contexts");
-            modelBuilder.Entity<Context>().HasKey(t => t.Id);
-            modelBuilder.Entity<Context>().Ignore(t => t.ExpressionsList);
-            modelBuilder.Entity<Context>().Ignore(t => t.InversedPolishQueue);
-            modelBuilder.Entity<Context>().Ignore(t => t.CurrentStringToMatchWithTree);
-            modelBuilder.Entity<Context>().Ignore(t => t.Root);
-            modelBuilder.Entity<Context>().Ignore(t => t.Root);
-            modelBuilder.Entity<Context>().Ignore(t => t.Root);
-            modelBuilder.Entity<Context>().Ignore(t => t.Root);
-            modelBuilder.Entity<Context>().Ignore(t => t.Root);
+            
         }
 
         #endregion
