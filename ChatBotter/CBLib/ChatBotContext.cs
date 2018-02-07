0e46cd4bc8e80d00de8a68bbde2e3ccfcaebe62c
Alexey Ryumin <aryumin@aryumin.ru>
2018-02-07 22:30:43 +0300

    additions of ContextWrapper to map context to db

--- ChatBotter/CBLib/ChatBotContext.cs
+++ ChatBotter/CBLib/ChatBotContext.cs
@@ -11,20 +11,31 @@ namespace CBLib
     {
         public ChatBotContext(DbContextOptions<ChatBotContext> options) : base(options)
         {
-
+            
         }
 
         public DbSet<TheProject> TheProjects { get; set; }
         public DbSet<BotResponse> BotResponses { get; set; }
         public DbSet<Context> Contexts { get; set; }
 
-        #region mappings
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            //modelBuilder.Entity<Context>().ToTable("Contexts");
+            //modelBuilder.Entity<Context>().Ignore(b => b.CurrentStringToMatchWithTree);
+            //modelBuilder.Entity<Context>().Ignore(b => b.ExpressionsList);
+            //modelBuilder.Entity<Context>().Ignore(b => b.InversedPolishQueue);
+            //modelBuilder.Entity<Context>().Ignore(b => b.Root);
+
+            //modelBuilder.Entity<Context>().Property(b => b.Id).HasColumnName("Id");
+            //modelBuilder.Entity<Context>().Property(b => b.Title).HasColumnName("Title");
+            //modelBuilder.Entity<Context>().Property(b => b.Priority).HasColumnName("Priority");
+            //modelBuilder.Entity<Context>().Property(b => b.ProjectId).HasColumnName("ProjectId");
+            //modelBuilder.Entity<Context>().Property(b => b.Root.ResStringExpression).HasColumnName("ResStringExpression");
         }
 
-        #endregion
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("mysql_conn");
+        }
     }
 }
---
+++ ChatBotter/CBLib/Entities/ContextWrapper.cs
@@ -0,0 +1,45 @@
+﻿using SamplesToTextsMatcher;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CBLib.Entities
+{
+    /// <summary>
+    /// Wrapper around the context.
+    /// </summary>
+    [Table("Contexts")]
+    public class ContextWrapper
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public int Priority { get; set; }
+
+        public string ExpressionStr { get; set; }
+
+        [NotMapped]
+        public Context Ctx { get; private set; }
+
+        /// <summary>
+        /// It is used to create real Context object using ExpressionStr.
+        /// </summary>
+        /// <param name="parser"></param>
+        public void InitExistingContext(AbstractPatternParser parser)
+        {
+            this.Ctx = new Context(this.ExpressionStr, parser);
+        }
+
+        /// <summary>
+        /// It is used to create new Context (e.g. before saving ContextWrapper object to database).
+        /// </summary>
+        public void InitNewContext()
+        {
+            //TODO here should be used main Context constructor and ExpressionStr prop set.
+        }
+    }
+}
