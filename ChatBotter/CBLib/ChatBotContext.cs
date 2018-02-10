1725058fa03dc350422dd8ceeb13700f4ecb7258
Alexey Ryumin <aryumin@aryumin.ru>
2018-02-10 09:45:28 +0300

    context's wrapper added

--- ChatBotter/CBLib/ChatBotContext.cs
+++ ChatBotter/CBLib/ChatBotContext.cs
@@ -18,21 +18,6 @@ namespace CBLib
         public DbSet<BotResponse> BotResponses { get; set; }
         public DbSet<Context> Contexts { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            //modelBuilder.Entity<Context>().ToTable("Contexts");
-            //modelBuilder.Entity<Context>().Ignore(b => b.CurrentStringToMatchWithTree);
-            //modelBuilder.Entity<Context>().Ignore(b => b.ExpressionsList);
-            //modelBuilder.Entity<Context>().Ignore(b => b.InversedPolishQueue);
-            //modelBuilder.Entity<Context>().Ignore(b => b.Root);
-
-            //modelBuilder.Entity<Context>().Property(b => b.Id).HasColumnName("Id");
-            //modelBuilder.Entity<Context>().Property(b => b.Title).HasColumnName("Title");
-            //modelBuilder.Entity<Context>().Property(b => b.Priority).HasColumnName("Priority");
-            //modelBuilder.Entity<Context>().Property(b => b.ProjectId).HasColumnName("ProjectId");
-            //modelBuilder.Entity<Context>().Property(b => b.Root.ResStringExpression).HasColumnName("ResStringExpression");
-        }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySql("mysql_conn");
