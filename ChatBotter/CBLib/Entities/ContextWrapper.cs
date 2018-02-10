75a8c0f1f47419f2950e94639610a44c7ef09d86
Alexey Ryumin <aryumin@aryumin.ru>
2018-02-10 10:33:13 +0300

    Updates of context wrapper class - mappings and initialization of context prop

--- ChatBotter/CBLib/Entities/ContextWrapper.cs
+++ ChatBotter/CBLib/Entities/ContextWrapper.cs
@@ -1,6 +1,8 @@
 ﻿using SamplesToTextsMatcher;
+using SamplesToTextsMatcher.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -12,15 +14,32 @@ namespace CBLib.Entities
     [Table("Contexts")]
     public class ContextWrapper
     {
+        [Key]
+        [Column("Id")]
         public int Id { get; set; }
 
+        [Column("Title")]
         public string Title { get; set; }
 
+        [Column("ProjectId")]
         public int ProjectId { get; set; }
 
+        [Column("Priority")]
         public int Priority { get; set; }
 
-        public string ExpressionStr { get; set; }
+        /// <summary>
+        /// Manually formed pattern.
+        /// </summary>
+        [Column("ExpressionRawStr")]
+        public string ExpressionRawStr { get; set; }
+
+        /// <summary>
+        /// Pattern formed using raw pattern interpretation
+        /// (with terms forms, all brackets for binary operations).
+        /// This pattern could be used to init existing Context.
+        /// </summary>
+        [Column("ExpressionResStr")]
+        public string ExpressionResStr { get; set; }
 
         [NotMapped]
         public Context Ctx { get; private set; }
@@ -31,15 +50,17 @@ namespace CBLib.Entities
         /// <param name="parser"></param>
         public void InitExistingContext(AbstractPatternParser parser)
         {
-            this.Ctx = new Context(this.ExpressionStr, parser);
+            this.Ctx = new Context(this.ExpressionResStr, parser);
         }
 
         /// <summary>
         /// It is used to create new Context (e.g. before saving ContextWrapper object to database).
         /// </summary>
-        public void InitNewContext()
+        public void InitNewContext(string pattern, AbstractPatternParser parser, AbstractMorfDictionary dict, 
+            bool shouldWorkWithTermsForms = false, int tokenFormsMaxNumberForAsterix = 30, Dictionary<string, LinkedList<Expression>> extras = null)
         {
-            //TODO here should be used main Context constructor and ExpressionStr prop set.
+            this.ExpressionRawStr = pattern;
+            this.Ctx = new Context(pattern, parser, dict, shouldWorkWithTermsForms, tokenFormsMaxNumberForAsterix, extras);
         }
     }
 }
