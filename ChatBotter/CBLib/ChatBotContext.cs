199b0f27f856d875c2a263090eb64fbeb2f6712c
Alexey Ryumin <aryumin@aryumin.ru>
2018-02-18 13:03:23 +0300

    config update and test in controller for greetings

--- ChatBotter/CBLib/ChatBotContext.cs
+++ ChatBotter/CBLib/ChatBotContext.cs
@@ -11,24 +11,21 @@ namespace CBLib
 {
     public class ChatBotContext : DbContext
     {
-        public ChatBotContext(DbContextOptions<ChatBotContext> options) : base(options)
-        {
-            
-        }
-
         public DbSet<TheProject> TheProjects { get; set; }
         public DbSet<BotResponse> BotResponses { get; set; }
         public DbSet<ContextWrapper> Contexts { get; set; }
+        public DbSet<Greeting> Greetings { get; set; }
+        public DbSet<Farewell> Farewells { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json");
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
             IConfiguration config = builder.Build();
-            var connString = config["ConnectionStrings[subsection:chatbotter_mysql_conn]"];
+            var connString = config.GetConnectionString("chatbotter_mysql_conn");
 
             optionsBuilder.UseMySql(connString);
 
---
+++ ChatBotter/ChatBotterWebApi/Controllers/GreetingsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CBLib;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBotterWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Greetings")]
+    public class GreetingsController : Controller
+    {
+        [Route("get")]
+        public IActionResult Get()
+        {
+            using (var db = new ChatBotContext())
+            {
+                var res = db.Greetings.ToList();
+                return Ok(res);
+            }
+        }
+    }
+}
