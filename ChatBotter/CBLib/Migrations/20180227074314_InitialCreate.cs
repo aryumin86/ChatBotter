047054f8fdab0110b6f7972c23c39ef07134442c
aryumin@aryumin.ru <Malinka3853.>
2018-04-15 09:58:13 +0300

    upd

---
+++ ChatBotter/CBLib/Migrations/20180227074314_InitialCreate.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CBLib.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    OwnerId = table.Column<string>(nullable: true),
+                    ProjectDescription = table.Column<string>(nullable: true),
+                    ProjectTitle = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contexts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ExpressionRawStr = table.Column<string>(nullable: true),
+                    ExpressionResStr = table.Column<string>(nullable: true),
+                    Priority = table.Column<int>(nullable: false),
+                    ProjectId = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contexts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contexts_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Farewells",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MainFarewell = table.Column<string>(nullable: true),
+                    ProjectId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Farewells", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Farewells_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Greetings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MainGreeting = table.Column<string>(nullable: true),
+                    ProjectId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Greetings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Greetings_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BotResponses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PatternId = table.Column<int>(nullable: false),
+                    Priority = table.Column<int>(nullable: false),
+                    ResponseText = table.Column<string>(nullable: true),
+                    TheProjectId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BotResponses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BotResponses_Contexts_PatternId",
+                        column: x => x.PatternId,
+                        principalTable: "Contexts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BotResponses_Projects_TheProjectId",
+                        column: x => x.TheProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BotResponses_PatternId",
+                table: "BotResponses",
+                column: "PatternId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BotResponses_TheProjectId",
+                table: "BotResponses",
+                column: "TheProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contexts_ProjectId",
+                table: "Contexts",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Farewells_ProjectId",
+                table: "Farewells",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Greetings_ProjectId",
+                table: "Greetings",
+                column: "ProjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BotResponses");
+
+            migrationBuilder.DropTable(
+                name: "Farewells");
+
+            migrationBuilder.DropTable(
+                name: "Greetings");
+
+            migrationBuilder.DropTable(
+                name: "Contexts");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
